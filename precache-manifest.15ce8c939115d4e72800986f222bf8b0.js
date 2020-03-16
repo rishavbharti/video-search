2adbd4e606419b9fa03057511cc56d25fe5ffddf
Rishav Bharti <rishavbharti21@gmail.com>
2020-03-16 13:10:15 +0530

    Updates

---
+++ precache-manifest.15ce8c939115d4e72800986f222bf8b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fea7572c06f10461087e1ad5ac330f9",
+    "url": "/video-search/index.html"
+  },
+  {
+    "revision": "16cc865756bffadc7050",
+    "url": "/video-search/static/css/main.46f49bac.chunk.css"
+  },
+  {
+    "revision": "26ab209cc497558ec13a",
+    "url": "/video-search/static/js/2.81b4db64.chunk.js"
+  },
+  {
+    "revision": "59fc29711a22a223e9f602decc11b79f",
+    "url": "/video-search/static/js/2.81b4db64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16cc865756bffadc7050",
+    "url": "/video-search/static/js/main.4bb7c4a3.chunk.js"
+  },
+  {
+    "revision": "6e35617755bed976253e",
+    "url": "/video-search/static/js/runtime-main.f4fb0d58.js"
+  },
+  {
+    "revision": "07b8560a6a6eb7155b80b0c599e6874e",
+    "url": "/video-search/static/media/YTLogo.07b8560a.png"
+  }
+]);
